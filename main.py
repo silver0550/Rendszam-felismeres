73fd5835062be496d345144c8050c298a0db9f0f
Gacs Péter <g.silver.014@gmail.com>
2022-03-15 13:43:41 +0100

    Előfeldolgozás, hiányzik: eredmény tesztelés, finomítás

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-import cv2
+import cv2 as cv
 
-#kép megnyitása
 
-img = cv2.imread('rszimg/rsz_test_1.jpg')
-cv2.imshow('test', img)
-cv2.waitKey(0)
+def show_Lplate(picture):
+    'rendszám-tábla detektálása ( eredményként egy maszkot ad vissza )'
+
+    # Kép megnyitása -> szürke árnyalat-> átméretezés
+    img = cv.imread(picture, cv.IMREAD_COLOR)
+    img = cv.resize(img, (800, 800))
+    img_act = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Zajcsökkentés
+    img_act = cv.bilateralFilter(img_act, 5, 100, 500)
+
+    # Éldetektálás
+    img_act = cv.Canny(img_act, 0, 250, None)
+
+    # Contúrok detektálása
+    contours, hierarchy = cv.findContours(img_act, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contureSizes = []
+
+    for i in range(len(contours)):
+        size = cv.contourArea(contours[i])
+        contureSizes.append((size, i))
+    contureSizes.sort(reverse=True)
+
+    # találatok tesztelése
+    chek: int
+    for i in range(10):
+        chek = contureSizes[i][1]
+        break
+
+    #Rendszám kirajzolása, maszk létrehozása
+    cv.drawContours(img, contours, chek, (0, 0, 255), -1)
+
+
+    return img
+
+
+
+if __name__ == "__main__":
+    cv.imshow('Picure', show_Lplate('rszimg/rsz_test_3.jpg'))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
